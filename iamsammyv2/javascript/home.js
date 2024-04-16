951ba7d331d66903a2da3d2e01e49d0df58d5a9e
WEBSTAR <159607706+syoungcod@users.noreply.github.com>
2024-04-16 23:07:22 +0200

    Delete iamsammyv2 directory

--- iamsammyv2/javascript/home.js
+++
@@ -1,9 +0,0 @@
-function work() {
-    const sidebar = document.querySelector ('.pic')
-    sidebar.style.display = 'flex'
-}
-
-function hide() {
-    const sidebar = document.querySelector ('.pic')
-    sidebar.style.display = 'none'
-}
