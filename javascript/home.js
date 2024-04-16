f4d04967c7af33b035a3ec123cb736dd26d7a066
WEBSTAR <159607706+syoungcod@users.noreply.github.com>
2024-04-16 23:08:48 +0200

    Add files via upload

---
+++ javascript/home.js
@@ -0,0 +1,9 @@
+function work() {
+    const sidebar = document.querySelector ('.pic')
+    sidebar.style.display = 'flex'
+}
+
+function hide() {
+    const sidebar = document.querySelector ('.pic')
+    sidebar.style.display = 'none'
+}
